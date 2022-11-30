985fd3f73b9c734521873d2534c475b91ca54625
ymyt <hope09476@gmail.com>
2022-11-30 11:55:44 +0600

    admin, search, filter

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
 from .models import Post
 from .serializers import PostSerializer
+
+User = get_user_model()
 
 
 @api_view(['GET'])
@@ -35,3 +38,20 @@ def delete_post(requests, id):
     post = get_object_or_404(Post, id=id)
     post.delete()
     return Response(status=204)
+
+
+@api_view(['GET'])
+def filter_by_user(requests, u_id):
+    # queryset = Post.objects.filter(author__id=u_id)
+    author = get_object_or_404(User, id=u_id)
+    queryset = Post.objects.filter(author=author)
+    serializer = PostSerializer(queryset, many=True)
+    return Response(serializer.data, status=200)
+
+@api_view(['Get'])
+def search(requests):
+    q = requests.query_params.get('q')
+    queryset = Post.objects.filter(body__icontains=q)
+    serializer = PostSerializer(queryset, many=True)
+    return Response(serializer.data, status=200)
+
